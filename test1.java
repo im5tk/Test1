ed262a665b0d575bff9a021e1d8c83ae64504614
im5tk <38789451+im5tk@users.noreply.github.com>
2018-08-25 18:44:45 +0900

    version1

---
+++ test1.java
@@ -0,0 +1,38 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class test1 {
+	public static void main(String[] args){
+		try{
+			// urlnameの「key=」の後にAPI_keyを入れてください
+			String urlname = "https://sheets.googleapis.com/v4/spreadsheets/"
+					+ "11BCnspCt2Mut3nhc4WMY6CYTd0zF9C3eCzsk1AEpKLM/values/"
+					+ "A1:E6?key=AIzaSyDwxpicDSa3GBcLJmgE1yxdtjYpIJFogcA";
+			URL url = new URL(urlname);
+			Object content = url.getContent();
+			if(content instanceof InputStream){
+				BufferedReader bf = new BufferedReader(new InputStreamReader((InputStream)content));
+				String line;
+				ArrayList<String> array = new ArrayList<String>();
+				while((line = bf.readLine()) != null){
+					array.add(line);
+				}
+				for(int j = 0;j < (array.size()-6)/7;j++){
+					for(int i = 0;i < 4;i++){
+						System.out.print(array.get(i+7*j+5).replace("\"","\'").replace(" ", ""));
+					}
+					System.out.println("");
+				}
+			}
+		}catch(IOException e){
+			System.out.println(e);
+			System.exit(-1);
+		}
+	}
+}
